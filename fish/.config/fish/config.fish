49b6f3ab8781f981b7c2dba18dbe8f1c792f367a
Maximiliano Guzenski <maxguzenski@gmail.com>
2024-12-31 20:09:06 -0300

    updates

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+zoxide init fish | source
+fzf --fish | source
+mise activate fish | source
+
+alias cd="z"
+alias c="clear"
+
+alias ls="eza --icons=always --group-directories-first"
+alias ll="ls -lh"
+alias la="ls -lah"
+alias lt="ls -ha -TI .git"
+
+alias vim="nvim"
+alias vi="nvim"
+alias v="nvim"
+#alias nvimdiff="nvim -d"
+
+alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+
+export EDITOR="nvim"
+
+set XDG_CONFIG_HOME "$HOME/.config"
+set fish_greeting ""
