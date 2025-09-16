a999bf36b09b29f1f4f0b7a1ffdc90fbbfdcee9d
Maximiliano Guzenski <maxguzenski@gmail.com>
2025-09-15 21:45:43 -0300

    move blink to default preset

--- lazyvim/.config/nvim/lua/plugins/blink.lua
+++ lazyvim/.config/nvim/lua/plugins/blink.lua
@@ -2,13 +2,14 @@ return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
-      preset = "enter",
-
-      ["<Tab>"] = { "select_next", "fallback" },
-      ["<S-Tab>"] = { "select_prev", "fallback" },
-
-      ["<C-n>"] = { "snippet_forward", "fallback_to_mappings" },
-      ["<C-p>"] = { "snippet_backward", "fallback_to_mappings" },
+      preset = "default",
+      -- preset = "enter",
+      --
+      -- ["<Tab>"] = { "select_next", "fallback" },
+      -- ["<S-Tab>"] = { "select_prev", "fallback" },
+      --
+      -- ["<C-n>"] = { "snippet_forward", "fallback_to_mappings" },
+      -- ["<C-p>"] = { "snippet_backward", "fallback_to_mappings" },
     },
     completion = {
       list = { selection = { preselect = false, auto_insert = false } },
