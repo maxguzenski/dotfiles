3cadc6f72d152a8a92a5cf3115e802549a329efe
Maximiliano Guzenski <maxguzenski@gmail.com>
2025-09-27 16:00:14 -0300

    update unomarchy

--- unmarchy.sh
+++ unmarchy.sh
@@ -8,3 +8,11 @@ yay -Rcs \
 omarchy-webapp-remove HEY
 omarchy-webapp-remove Basecamp
 omarchy-webapp-remove "Google contacts"
+
+mise use -g node@24
+
+pacman -Sy \
+  stow \
+  zellij \
+  ast-grep \
+  firefox
