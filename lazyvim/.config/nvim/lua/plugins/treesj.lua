3b35895eea8dda3257d3587484bf2ff7daf9ad37
Maximiliano Guzenski <maxguzenski@gmail.com>
2025-12-08 20:59:18 -0300

    updates

---
+++ lazyvim/.config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,8 @@
+return {
+  "Wansmer/treesj",
+  dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+  config = function()
+    require("treesj").setup({ use_default_keymaps = false })
+    vim.keymap.set("n", "<leader>m", require("treesj").toggle, { desc = "Join or split list/map/etc" })
+  end,
+}
