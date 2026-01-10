882ddd154c8aebebf61b589b7aedaae97a0e281f
Maximiliano Guzenski <maxguzenski@gmail.com>
2026-01-10 03:04:27 -0300

    feat: yazi install

---
+++ setup-yazi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ya pkg install KKV9/compress
+ya pkg install yazi-rs/plugins:piper
+ya pkg install yazi-rs/plugins:toggle-pane
+ya pkg install yazi-rs/flavors:catppuccin-macchiato
+ya pkg install yazi-rs/flavors:catppuccin-mocha
+
+ya pkg upgrade
