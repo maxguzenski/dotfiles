7a539191723199cfcacd858e97cf00a8ecf5952a
Maximiliano Guzenski <maxguzenski@gmail.com>
2026-05-01 03:58:29 -0300

    fix zed keymap , ghostty and lazyvim config

--- lazyvim/lua/plugins/snacks.lua
+++ lazyvim/lua/plugins/snacks.lua
@@ -16,31 +16,15 @@ return {
         file = { filename_first = true, truncate = 120 },
       },
       layout = {
-        preview = true,
+        preview = false,
+        preset = "ivy",
         -- preset = function()
-        --   return vim.o.columns >= 200 and "default" or "myvertical"
+        --   return vim.o.columns >= 200 and "" or "myvertical"
         -- end,
       },
       layouts = {
         default = {},
         vertical = {},
-
-        myvertical = {
-          layout = {
-            backdrop = false,
-            width = 0.65,
-            min_width = 80,
-            height = 0.65,
-            min_height = 30,
-            box = "vertical",
-            border = "rounded",
-            title = "{title} {live} {flags}",
-            title_pos = "center",
-            { win = "input", height = 1, border = "bottom" },
-            { win = "list", border = "none" },
-            { win = "preview", title = "{preview}", height = 0.5, border = "top" },
-          },
-        },
       },
     },
   },
